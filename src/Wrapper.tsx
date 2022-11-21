fd8fef31ed6d588d11655f1e1d7c3ffb1d7850a9
lautaro.andreani <lautaro.andreani@coderhouse.com>
2022-11-21 15:15:40 -0300

    change: move main tag to wrapper

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -3,12 +3,13 @@ import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
 import NavBar from './components/Navbar'
 
+import style from './App.module.css'
 export default function Wrapper() {
 	return (
-		<>
+		<main className={style.container}>
 			<NavBar />
 			<Outlet />
 			<Footer />
-		</>
+		</main>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/routes'
 
-import style from './App.module.css'
 import '@fontsource/montserrat/400.css'
 import '@fontsource/montserrat/700.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<main className={style.container}>
-			<RouterProvider router={router} />
-		</main>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 )
