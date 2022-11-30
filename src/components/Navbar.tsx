e4ff4a56f12b2059f488d2d09d4ee02449efff01
LautaAndreani <lautaroandreani2016@gmail.com>
2022-11-30 20:16:15 -0300

    fix(styles): media queries & layout adjust

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
 import style from './NavBar.module.css'
 
 export default function NavBar() {
 	return (
 		<nav className={style.container}>
-			<img src="/logos/CatwikiLogo.svg" alt="cat wiki logo" />
+			<Link to='/'>
+				<img src="/logos/CatwikiLogo.svg" alt="cat wiki logo" />
+			</Link>
 		</nav>
 	)
 }
